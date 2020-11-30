2da37028a3a2debc685eae3b1f205adb6063baaf
Dylan Fox <dylan.fox@colorado.edu>
2020-11-30 12:41:23 -0800

    adding everything to read file path from file, and execute that file

--- kernel_space_invocation/example_program.c
+++ kernel_space_invocation/example_program.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+#define PATH "/home/dylanfox/Desktop/csci5573-project/kernel_space_invocation/"
+
 
 
 int main() {
-   FILE *fp;
-   fp = fopen("/home/dylan/Desktop/OS_project_examples/test.txt", "w");
-   fprintf(fp, "Testing!...\n");
-   fclose(fp);
+  FILE *fp;
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+   } else {
+       perror("getcwd() error");
+       return 1;
+   }
+  fp = fopen(PATH"test.txt", "w+");
+  fprintf(fp, "%s\n", cwd);
+  fclose(fp);
+  return 0;
 }
--- kernel_space_invocation/lkm.c
+++ kernel_space_invocation/lkm.c
@@ -3,13 +3,16 @@
 #include <linux/init.h>
 #include <linux/kmod.h>
 #include <linux/fs.h>                   /* file ops struct*/
-
+#include <linux/limits.h>
+#include <linux/mm.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dylan Fox");
 
 #define SUCCESS 0
 #define DEVICE_NAME "call_usr_example"
+#define USER_NAME "dylanfox"
+#define PROJECT_PATH "/home/dylanfox/Desktop/csci5573-project/kernel_space_invocation/"
 
 static int Major;
 static int Device_Open = 0;
@@ -31,22 +34,47 @@ static int device_release(struct inode *inode, struct file *file) {
 
 static ssize_t device_read(struct file *filp,char *buffer, size_t length, loff_t * offset) {
 // see https://developer.ibm.com/technologies/linux/articles/l-user-space-apps/
-  char *argv[] = { "/home/dylan/Desktop/OS_project_examples/example_program.o", NULL };
+  int return_val;
   static char *envp[] = {
     "SHELL=/bin/bash",
-    "HOME=/home/dylan",
-    "USER=dylan",
+    "HOME=/home/"USER_NAME"/",
+    "USER="USER_NAME,
     "TERM=xterm-256color",
     "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
     "DISPLAY=:0",
     "GNOME_SHELL_SESSION_MODE=ubuntu",
-    "USERNAME=dylan",
-    "PWD=/home/dylan/",
-    NULL };
+    "USERNAME="USER_NAME,
+    "PWD="PROJECT_PATH,
+    NULL
+  };
+
+  char *path_to_exec = kvmalloc(PATH_MAX, GFP_KERNEL);
+  char *argv[2];
+  struct file* path_file = filp_open(PROJECT_PATH"path.txt",  O_RDONLY, 0666);
+  char byte_read[1];
+  loff_t position = 0;
+  int num_bytes_read = 0;
+  while (num_bytes_read < PATH_MAX) {
+    kernel_read(path_file, byte_read, 1, &position);
+    if (byte_read[0] == '\n') {
+      break;
+    }
+    else {
+      path_to_exec[num_bytes_read] = byte_read[0];
+      num_bytes_read += 1;
+    }
+  }
+  filp_close(path_file, NULL);
+  argv[0] = path_to_exec;
+  // printk(KERN_ALERT"Argv version: %s", argv[0]);
+  argv[1] = NULL;
+
+
   // can wait for process to complete, go with waiting, or wait for it to be invoked
   // change UMH_WAIT_PROC to UMH_NO_WAIT or UMH_WAIT_EXEC
-  int return_val = call_usermodehelper( argv[0], argv, envp, UMH_WAIT_PROC );
-  printk(KERN_ALERT "return val: %d", return_val);
+  return_val = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC );
+  kvfree(path_to_exec);
+  printk(KERN_ALERT "return val: %d\n", return_val);
   return SUCCESS;
 }
 
