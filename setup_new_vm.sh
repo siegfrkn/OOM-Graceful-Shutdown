e22668f12e6218ca597e136cfebd198f404fd6a4
Dylan Fox <dylan.fox@colorado.edu>
2020-12-02 15:41:10 -0700

    addign script to setup vm

---
+++ setup_new_vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# get kernel source in background while installing stuff.
+(wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.4.1.tar.gz \
+&& sudo tar -xvf linux-5.4.1.tar.gz -C/usr/src) &
+
+#packages for kernel compilation and sshing into vm.
+sudo apt-get install \
+gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server
+&& sudo apt-get update && sudo apt-get upgrade && sudo systemctl enable ssh
