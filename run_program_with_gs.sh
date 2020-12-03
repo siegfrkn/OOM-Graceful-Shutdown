1acc2c089fbd13f07bc0b53d9e7681c81a5985c9
Dylan Fox <dylan.fox@colorado.edu>
2020-12-03 10:19:33 -0700

    adding script to run program and echo pid to gs file

---
+++ run_program_with_gs.sh
@@ -0,0 +1,18 @@
+if [ "$#" -ne 2 ]
+  then
+    echo "Usage ./run_program_with_gs.sh <program_path> <gs_executable_path>"
+    echo "Ex. ./run_program_with_gs.sh ./test.o /home/user/Desktop/graceful_shutdown.o"
+    echo "ERROR: More or less than two parameters supplied."
+    exit 1
+  fi
+if [ "${2:0:1}" != "/" ]
+  then
+    echo "${2:0:1}"
+    echo "Warning! Graceful Shutdown paths must be absolute!"
+    echo "The first character of your input isn't a \"/\", so it probably isn't an absolute path."
+    echo "Continuing, but if the gs path isn't absolute it won't work."
+  fi
+
+current_dir=$(pwd)
+echo "Starting process ${1} with oom graceful shutdown procedure of: ${2}"
+sudo /bin/bash -c 'echo "$(echo $$) ${1}"  >> /proc/graceful_shutdown && exec ${2}' $1 $2
