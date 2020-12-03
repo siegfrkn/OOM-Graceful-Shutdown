0a3ee7406c477e7d6e67c5093e810be3d0d83f83
Dylan Fox <dylan.fox@colorado.edu>
2020-12-03 11:43:23 -0700

    removing testing echo statements

--- copy_oom_kill_file_and_build.sh
+++ copy_oom_kill_file_and_build.sh
@@ -3,10 +3,14 @@ if [ "$#" -ne 2 ]
     echo "Usage ./copy_oom_kill_file_and_build.sh <linux_source_path> <number_of_processes_to_build_with>"
     echo "Ex. ./copy_oom_kill_file_and_build.sh usr/src/linux-5.4/ 9"
     echo "ERROR: More or less than two parameters supplied."
+    exit 1
   fi
 
 mkdir -p old_oom_kill/
-sudo mv "${1}/mm/oom_kill.c" ./old_oom_kill/ &>/dev/null
+
+num_old_files=$(ls -1 old_oom_kill/ | wc -l)
+sudo mv "${1}/mm/oom_kill.c" "./old_oom_kill/oom_kill.c-${num_old_files}" &>/dev/null
+
 sudo cp oom_kill.c "${1}/mm/"
 
 cd "${1}/" && sudo make "-j${2}" && sudo make modules_install install
