579db45dc25a833646fad6ce410ea233aa75a222
Dylan Fox <dylan.fox@colorado.edu>
2020-12-03 13:16:05 -0700

    updating build shell script to automatically find number of cores

--- copy_oom_kill_file_and_build.sh
+++ copy_oom_kill_file_and_build.sh
@@ -1,8 +1,8 @@
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 1 ]
   then
-    echo "Usage ./copy_oom_kill_file_and_build.sh <linux_source_path> <number_of_processes_to_build_with>"
-    echo "Ex. ./copy_oom_kill_file_and_build.sh usr/src/linux-5.4/ 9"
-    echo "ERROR: More or less than two parameters supplied."
+    echo "Usage ./copy_oom_kill_file_and_build.sh <linux_source_path>"
+    echo "Ex. ./copy_oom_kill_file_and_build.sh usr/src/linux-5.4/"
+    echo "ERROR: More or less than one parameters supplied."
     exit 1
   fi
 
@@ -13,4 +13,5 @@ sudo mv "${1}/mm/oom_kill.c" "./old_oom_kill/oom_kill.c-${num_old_files}" &>/dev
 
 sudo cp oom_kill.c "${1}/mm/"
 
-cd "${1}/" && sudo make "-j${2}" && sudo make modules_install install
+num_processors=$(grep -c ^processor /proc/cpuinfo)
+cd "${1}/" && sudo make "-j${num_processors}" && sudo make modules_install install
