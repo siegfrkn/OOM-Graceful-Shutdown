d7a2ede7b49365496134a6f936dbb68236cc940c
siegfrkn <katrina.siegfried@gmail.com>
2020-12-04 00:01:36 -0700

    update kernel to build with needed cgroup flags and update scripts accordingly

--- setup_initial_running_env.sh
+++ setup_initial_running_env.sh
@@ -7,36 +7,38 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 
 # add the lkm for the proc directory to run at boot every time
-echo "Setting /proc/graceful_shutdown to initialize on every boot....."
-echo 'proc_create_module ' >> /etc/modules
+echo "Setting /proc/graceful_shutdown to initialize on every boot....." &&
+echo 'proc_create_module ' >> /etc/modules &&
 
 # enable the memory management if not already enabled
-echo "Setting up graceful_shutdown cgroups....."
+echo "Setting up graceful_shutdown cgroups....." &&
 echo '# Uncomment to enable memory management' >> /etc/default/grub &&
-echo 'GRUB_CMDLINE_LINUX="quiet cgroup_enable=memory swapaccount=1"' >> /etc/default/grub &&
-sudo update-grub
+echo 'GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"' >> /etc/default/grub &&
+sudo update-grub &&
+sudo touch /etc/default/grub.d/mem_cgroup
+echo cgroup/cgroup_grub >> /etc/default/grub.d
 
 #use template for global config to create a global config
-sudo cp /usr/share/doc/cgroup-tools/examples/cgred.conf /etc/
+sudo cp /usr/share/doc/cgroup-tools/examples/cgred.conf /etc/ &&
 
 #create file to specify groups
-echo sudo touch /etc/cgconfig.conf
-cat cgroup/cgroup_cgconfig.conf >> /etc/cgconfig.conf
+sudo touch /etc/cgconfig.conf &&
+cat cgroup/cgroup_cgconfig.conf >> /etc/cgconfig.conf &&
 
 # create file to allocate programs to groups
 sudo touch /etc/cgrules.conf &&
-cat cgroup/cgroup_rules.conf >> /etc/cgrules.conf
+cat cgroup/cgroup_rules.conf >> /etc/cgrules.conf &&
 
 # apply the rules, this needs to happen after all the above
 sudo cgconfigparser -l /etc/cgconfig.conf &&
 sudo cgrulesengd
 
 # create the systemd files for config and rules
-echo "Setting up cgroups to start at boot....."
+echo "Setting up cgroups to start at boot....." &&
 sudo touch /etc/systemd/system/cgconfigparser.service &&
-cat cgroup/cgroup_systemd_config >> /etc/systemd/system/cgconfigparser.service
+cat cgroup/cgroup_systemd_config >> /etc/systemd/system/cgconfigparser.service &&
 sudo touch /etc/systemd/system/cgrulesparser.service &&
-cat cgroup/cgroup_systemd_rules >> /etc/systemd/system/cgrulesparser.service
+cat cgroup/cgroup_systemd_rules >> /etc/systemd/system/cgrulesparser.service &&
 
 # restart systemctl to apply the changes above
 sudo systemctl daemon-reload &&
--- setup_new_vm.sh
+++ setup_new_vm.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+LINUX_SRC_DIR='/usr/src/linux-5.4.1'
+
 # get kernel source in background while installing stuff.
 (wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.4.1.tar.gz \
-&& sudo tar -xvf linux-5.4.1.tar.gz -C/usr/src) &
+&& sudo tar -xvf linux-5.4.1.tar.gz -C/usr/src) > install.log 2>&1 &
 
 #packages for kernel compilation and sshing into vm.
-sudo apt-get install gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server &&
+sudo apt-get install gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server cgroup-tools &&
 sudo apt-get update &&
 sudo apt-get upgrade &&
 sudo systemctl enable ssh
 
-cd /usr/src/linux-5.4.1 && sudo make defconfig
+sudo cp kernel.config $LINUX_SRC_DIR/.config &&
+cd "$LINUX_SRC_DIR" &&
+sudo make defconfig
