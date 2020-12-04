e29328a40f02acd17e68bfabc945281f21aae5ee
siegfrkn <katrina.siegfried@gmail.com>
2020-12-03 17:03:46 -0700

    tweak a few syntax issues in the script

--- setup_new_vm.sh
+++ setup_new_vm.sh
@@ -5,8 +5,9 @@
 && sudo tar -xvf linux-5.4.1.tar.gz -C/usr/src) &
 
 #packages for kernel compilation and sshing into vm.
-sudo apt-get install \
-gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server
-&& sudo apt-get update && sudo apt-get upgrade && sudo systemctl enable ssh
+sudo apt-get install gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server &&
+sudo apt-get update &&
+sudo apt-get upgrade &&
+sudo systemctl enable ssh
 
 cd /usr/src/linux-5.4.1 && sudo make defconfig
