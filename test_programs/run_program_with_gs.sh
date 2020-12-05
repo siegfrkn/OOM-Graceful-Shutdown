e1985a673267ae282016c123f5e5677c09aec5bb
Dylan Fox <dylan.fox@colorado.edu>
2020-12-05 07:55:45 -0700

    changing runner shell script to us cgexec

--- test_programs/run_program_with_gs.sh
+++ test_programs/run_program_with_gs.sh
@@ -1,4 +1,6 @@
-set -x
+#set -x
+CGROUP_NAME="memlimit"
+
 if [ "$#" -ne 2 ]
   then
     echo "Usage ./run_program_with_gs.sh <program_path> <gs_executable_path>"
@@ -15,4 +17,4 @@ if [ "${2:0:1}" != "/" ]
   fi
 
 echo "Starting process ${1} with oom graceful shutdown procedure of: ${2}"
-/bin/bash -c 'sudo echo "$(echo $$) ${0}"  >> /proc/graceful_shutdown && exec ${1}' $2 $1
+sudo /bin/bash -c 'echo "$(echo $$) ${0}"  >> /proc/graceful_shutdown && cgexec -g memory:${1} ${2}' $2 $CGROUP_NAME $1
