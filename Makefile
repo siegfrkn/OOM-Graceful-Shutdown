1339ffc9fb70c122ecd00f66df7073d29309e6d5
siegfrkn <katrina.siegfried@gmail.com>
2020-12-06 11:51:10 -0700

    update makefile to handle clean and integrate setup_new_vm.sh so all you have to do is make all

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ LINUX_SOURCE_DIR:=/usr/src/linux-5.4.1/
 
 all: proc tests linux_built.txt
 
-linux_built.txt: oom_kill.c
+setup_new_vm:
+	/bin/bash -c "sudo ./setup_new_vm.sh"
+
+
+linux_built.txt:  setup_new_vm oom_kill.c
 	/bin/bash -c "sudo ./copy_oom_kill_file_and_build.sh $(LINUX_SOURCE_DIR)"
 	rm -f "linux_built.txt"
 	touch "linux_built.txt"
@@ -12,6 +16,12 @@ linux_built.txt: oom_kill.c
 tests:
 	$(MAKE) -C test_programs
 
-proc:
+proc: linux_built.txt
 	$(MAKE) -C proc_filesystem
 	$(MAKE) install -C proc_filesystem
+
+clean:
+	rm -r linux-5.4.1*
+	sudo rm -r $(LINUX_SOURCE_DIR)
+	$(MAKE) -C proc_filesystem
+	$(MAKE) clean -C test_programs
--- setup_new_vm.sh
+++ setup_new_vm.sh
@@ -6,17 +6,23 @@ LINUX_SRC_DIR='/usr/src/linux-5.4.1'
 rm -f sys_install.log install.log
 
 # get kernel source in background while installing stuff.
+echo "Get kernel source...."
 (wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.4.1.tar.gz \
 && sudo tar -xvf linux-5.4.1.tar.gz -C/usr/src) > install.log 2>&1 &
 
 #packages for kernel compilation and sshing into vm.
+echo "Get necessary packages...."
 (sudo apt-get install gcc make libncurses5-dev bison flex libssl-dev libelf-dev openssh-server cgroup-tools -y &&
 sudo apt-get update -y &&
 sudo apt-get upgrade -y &&
 sudo systemctl enable ssh) > sys_install.log 2>&1 &
 
 # wait for both to finish, then setup config.
-echo "Waiting for both jobs to finish!"
+echo "Waiting for both jobs to finish...."
 wait $(jobs -p)
+echo "Replace defconfig with custom config file...."
 cd "$LINUX_SRC_DIR" && sudo make defconfig
 cd - && sudo cp kernel.config $LINUX_SRC_DIR/.config
+
+echo "New VM setup complete!"
+sync
