ebf41d675f6c4573e373aaba38ef73b032f2bc21
siegfrkn <katrina.siegfried@gmail.com>
2020-12-06 18:21:19 -0700

    add additional timer output

--- oom_kill.c
+++ oom_kill.c
@@ -429,7 +429,7 @@ static void select_bad_process(struct oom_control *oc)
 	char *argv[2] = {NULL, NULL};
 	int pid;
 	char* gs_path;
-	long start_gs, end_gs, diff_gs;
+	long start_gs, end_gs, diff_gs_sec, diff_gs_ns;
 
 	if (is_memcg_oom(oc))
 		mem_cgroup_scan_tasks(oc->memcg, oom_evaluate_task, oc);
@@ -462,8 +462,10 @@ static void select_bad_process(struct oom_control *oc)
 		/* get ending timer for graceful shutdown and calculate diff */
 		end_gs = ktime_get_ns();
 		printk("oom timer: end at %ld nanoseconds\n", end_gs);
-		diff_gs = (end_gs - start_gs) / 1000000000;
-		printk("oom timer: gs diff %ld seconds\n", diff_gs);
+		diff_gs_ns = (end_gs - start_gs) / 1000000000;
+		printk("oom timer: gs diff %ld seconds\n", diff_gs_ns);
+		diff_gs_sec = end_gs - start_gs;
+		printk("oom timer: gs diff %ld seconds\n", diff_gs_sec);
 }
 
 static int dump_task(struct task_struct *p, void *arg)
